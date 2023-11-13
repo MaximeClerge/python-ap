910a5f7e1e49478f66b4aa105244607b732be0be
Maxime Clerge <maxime.clerge@hotmail.com>
2023-11-13 22:25:39 +0100

    Fruit

--- blank.py
+++ blank.py
@@ -1,15 +1,17 @@
 import pygame
 from time import sleep
+import random as rd
 
 WHITE = (255, 255, 255) #Couleur Blanche RGB
 BLACK = (0, 0, 0) #Couleur Noir RGB
 GREEN = (0, 255, 0) #Couleut Verte RGB
+RED = (255, 0, 0) #Couleut Rouge RGB
 LEN=300
 HEIGHT=400
 clockf=3 #Vitesse du jeux/vitesse de l'horloge
 TILE=20
 
-snake=[(5,10),(6,10),(7,10)]
+snake=[(5,10),(6,10),(7,10)] #Liste modélisant le serpent 
 
 HAUT = (0,-1)
 BAS = (0,1)
@@ -18,6 +20,13 @@ GAUCHE = (-1,0)
 
 snake_dir = DROIT
 snakehead = snake[-1]
+
+def spwan_new_fruit(): #fonction créant les coordonnée d'un fruit sur le plateau placée aléatoirement 
+    global fruit
+    fruit = [rd.randint(0,19),rd.randint(0,14)]
+    return 
+
+spwan_new_fruit()
 
 pygame.init()
 screen = pygame.display.set_mode((HEIGHT,LEN))
@@ -51,17 +60,20 @@ while True:
                 rect = pygame.Rect(left, top, TILE, TILE)  #Creation du pavage 
                 pygame.draw.rect(screen, BLACK, rect)  
 
+    fruitpxl = pygame.Rect(fruit[0]*TILE, fruit[1]*TILE, TILE, TILE) 
+    pygame.draw.rect(screen, RED, fruitpxl)
 
     snakehead=snake[-1]
     newsnakehead = [snakehead[0]+snake_dir[0],snakehead[1]+snake_dir[1]]
-    snake = snake[1:] + [newsnakehead]
 
-    snakdrw = pygame.Rect(snake[0][0]*TILE, snake[0][1]*TILE, TILE, TILE)
-    pygame.draw.rect(screen, GREEN, snakdrw)
-    snakdrw = pygame.Rect(snake[1][0]*TILE, snake[1][1]*TILE, TILE, TILE)
-    pygame.draw.rect(screen, GREEN, snakdrw)
-    snakdrw = pygame.Rect(snake[2][0]*TILE, snake[2][1]*TILE, TILE, TILE)
-    pygame.draw.rect(screen, GREEN, snakdrw)
+    if newsnakehead == fruit:
+        snake = snake + [newsnakehead]
+        spwan_new_fruit()
+    else :
+        snake = snake[1:] + [newsnakehead]
+    for i in range(len(snake)):
+        snakdrw = pygame.Rect(snake[i][0]*TILE, snake[i][1]*TILE, TILE, TILE)
+        pygame.draw.rect(screen, GREEN, snakdrw)
 
 
     pygame.display.update()

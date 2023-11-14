ed33f67581046b1c78e55d9f58df98e2bf273f5a
MaximeClerge <144037510+MaximeClerge@users.noreply.github.com>
2023-11-14 15:48:32 +0100

    12 command line arguments (#13)
    * Ajoute des args mais il faut enelver les cst
    
    * SANS CST

--- snake1.py
+++ snake1.py
@@ -1,27 +1,34 @@
 import pygame
 import random as rd
+import argparse
 
-COULEURBOARD1 = (255, 255, 255) #Couleur du plateau n1
-COULEURBOARD2 = (0, 0, 0) #Couleur du plateau n2
-COULEURSNAKE = (0, 255, 0) #Couleur du serpent
-COULEURSCOREBOARD = (255, 0, 0) #Couleur du score board
-COULEURFRUIT = (255, 0, 0) #Couleur des fruit
+parser = argparse.ArgumentParser(description='Commande permettant de regler les different parametre du jeux')
+parser.add_argument('--bg-color-1',default=(255,255,255) ,help="argument for the first color of the background checkerboard.")
+parser.add_argument('--bg-color-2', default=(0,0,0),help="argument for the second color of the background checkerboard.")
+parser.add_argument('--height', default=300 ,type=int,help="argument for the window height.")
+parser.add_argument('--width', default=400 ,type=int,help="argument for the window width.")
+parser.add_argument('--fps', default=6 ,type=int,help="argument for the number of frames per second.")
+parser.add_argument('--tile-size', default=20 ,type=int,help="argument the size of a square tile.")
+parser.add_argument('--fruit-color', default=(255, 0, 0),help="argument the size of a square tile.")
+parser.add_argument('--snake-length', default=3 ,type=int,help="argument the size of a square tile.")
+parser.add_argument('--snake-color', default=(0, 255, 0) ,help="argument the size of a square tile.")
 
-LEN=300 #largeur plateau
-HEIGHT=400 #hauteur plateau
-TILE=20 #taille des tuile
+args = parser.parse_args()
+print(args)
 
-CLOCKF=6 #Vitesse du jeux/vitesse de l'horloge
-
+#CONSTANTE
+COULEURSCOREBOARD = (255,0,0)#Couleur du score board
 TAILLEFONT=72 #taille du scoreboard
-PLACEMENTSCOREBOARD=(186,3) #Placement du scoreboard
+PLACEMENTSCOREBOARD=(0,0) #Placement du scoreboard
 
 HAUT = (0,-1) #Création des vecteur direction
 BAS = (0,1)
 DROITE = (1,0)
 GAUCHE = (-1,0)
+snake=[]
 
-snake=[(5,10),(6,10),(7,10)] #Liste modélisant le serpent 
+for i in range (args.snake_length):
+    snake+= [(i,0)] #Liste modélisant le serpent 
 
 snake_dir = DROITE #Inisialisation d'une direction de depart 
 snakehead = snake[-1] 
@@ -34,13 +41,13 @@ game = {
 
 def spwan_new_fruit(): #fonction créant les coordonnée d'un fruit sur le plateau placée aléatoirement 
     global fruit #Mise en place d'une variable global 
-    fruit = [rd.randint(0,HEIGHT/TILE-1),rd.randint(0,LEN/TILE-1)]
+    fruit = [rd.randint(0,args.height/args.tile_size-1),rd.randint(0,args.height/args.tile_size-1)]
     return 
 
 spwan_new_fruit()
 
 pygame.init()
-screen = pygame.display.set_mode((HEIGHT,LEN))
+screen = pygame.display.set_mode((args.width,args.height))
 clock = pygame.time.Clock()
 
 font = pygame.font.SysFont("Latex", TAILLEFONT) #Font du scoreboard
@@ -49,7 +56,7 @@ font = pygame.font.SysFont("Latex", TAILLEFONT) #Font du scoreboard
 if game['mode'] == "MODE_START": #Debut du jeux
     while True:
 
-        clock.tick(CLOCKF)
+        clock.tick(args.fps)
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -67,16 +74,16 @@ if game['mode'] == "MODE_START": #Debut du jeux
                 quit(0)
             pass
 
-        screen.fill(COULEURBOARD1)
+        screen.fill(args.bg_color_1)
         
-        for top in range(0,LEN,TILE):
-            for left in range (0,HEIGHT,TILE):
-                if (top + left )//20%2==0 :
-                    rect = pygame.Rect(left, top, TILE, TILE)  #Creation du pavage 
-                    pygame.draw.rect(screen, COULEURBOARD2, rect)  
+        for top in range(0,args.height,args.tile_size):
+            for left in range (0,args.height,args.tile_size):
+                if (top + left )//args.tile_size%2==0 :
+                    rect = pygame.Rect(left, top, args.tile_size, args.tile_size)  #Creation du pavage 
+                    pygame.draw.rect(screen, args.bg_color_2, rect)  
 
-        fruitpxl = pygame.Rect(fruit[0]*TILE, fruit[1]*TILE, TILE, TILE) 
-        pygame.draw.rect(screen, COULEURFRUIT, fruitpxl) #Dessin du fruit ROUGE 
+        fruitpxl = pygame.Rect(fruit[0]*args.tile_size, fruit[1]*args.tile_size, args.tile_size, args.tile_size) 
+        pygame.draw.rect(screen, args.fruit_color, fruitpxl) #Dessin du fruit ROUGE 
 
         snakehead = snake[-1]
         newsnakehead = [snakehead[0]+snake_dir[0],snakehead[1]+snake_dir[1]] 
@@ -88,8 +95,8 @@ if game['mode'] == "MODE_START": #Debut du jeux
         else :
             snake = snake[1:] + [newsnakehead]
         for i in range(len(snake)):
-            snakdrw = pygame.Rect(snake[i][0]*TILE, snake[i][1]*TILE, TILE, TILE)
-            pygame.draw.rect(screen, COULEURSNAKE, snakdrw)
+            snakdrw = pygame.Rect(snake[i][0]*args.tile_size, snake[i][1]*args.tile_size, args.tile_size, args.tile_size)
+            pygame.draw.rect(screen, args.snake_color, snakdrw)
 
         text = font.render(str(game['score']),1,COULEURSCOREBOARD) #Score board 
         screen.blit(text,PLACEMENTSCOREBOARD)   #Mise en place du texte sur l'ecran
